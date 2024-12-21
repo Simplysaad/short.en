51a88a584e04f1e0d22ea2553a3cea713cf4bbd8
Saad Idris <148543136+Simplysaad@users.noreply.github.com>
2024-12-21 15:35:18 +0100

    update Public/script.js

--- Public/script.js
+++ Public/script.js
@@ -1,51 +1,29 @@
-
-// const copyValue = (btn, text) => {
-//     btn.addEventListener("click", e => {
-//         e.preventDefault();
-//         navigator.clipboard
-//             .writeText(text.value)
-//             .then(() => {
-//                 console.log("text copied to clipboard");
-//             })
-//             .catch(err => {
-//                 console.error("failed to copy text", err);
-//             });
-
-//         btn.classList.add("text-success");
-//         btn.classList.replace("fa-clipboard", "fa-check");
-//     });
-// };
-
-// const originalUrlBtn = document.getElementById("originalUrlBtn");
-// const originalUrl = document.getElementById("originalUrl");
-
-// const shortUrlBtn = document.getElementById("shortUrlBtn");
-// const shortUrl = document.getElementById("shortUrl");
-
-
-// copyValue(shortUrlBtn, shortUrl);
-// copyValue(originalUrlBtn, originalUrl);
-
 const copyValue = (btn, inputElement) => {
     btn.addEventListener("click", e => {
         e.preventDefault();
-        
-        // Get the value of the input element
+
         const textToCopy = inputElement.value;
 
-        // Copy the text to the clipboard
         navigator.clipboard
-        .writeText(textToCopy)
+            .writeText(textToCopy)
             .then(() => {
                 console.log("Text copied to clipboard");
+
+                // Change button style to indicate success
+                let icon = btn.querySelector(".fa-clipboard");
+                btn.classList.add("text-success");
+                icon.classList.replace("fa-clipboard", "fa-check");
+
+                // Reset button icon after 2 seconds
+                setTimeout(() => {
+                    let icon = btn.querySelector(".fa-check");
+                    btn.classList.remove("text-success");
+                    icon.classList.replace("fa-check", "fa-clipboard");
+                }, 5000); // Reset after 5 seconds
             })
             .catch(err => {
                 console.error("Failed to copy text", err);
             });
-
-        // Change button style to indicate success
-        btn.classList.add("text-success");
-        btn.classList.replace("fa-clipboard", "fa-check");
     });
 };
 
