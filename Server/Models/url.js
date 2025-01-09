67c3fd3e1fe3bdb6884b76c819c3ea78b254a08a
Saad Idris <148543136+Simplysaad@users.noreply.github.com>
2025-01-09 16:49:48 +0100

    update Server/Models/url.js

--- Server/Models/url.js
+++ Server/Models/url.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
 const urlSchema = new mongoose.Schema({
-    originalUrl: {
-        type: String,
-        required: true
-    },
-    shortUrl: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now
-    }
+  originalUrl: {
+    type: String,
+    required: true
+  },
+  shortUrl: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  clickCount: {
+    type: Number,
+    default: 0
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  }
 });
 const url = new mongoose.model("Url", urlSchema)
 module.exports = url
